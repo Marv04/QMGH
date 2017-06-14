9ba4a5f130c7db8bfd9d7a56ffa08756aa0cc9f6
JohnAEX <grenda.jonathan@gmail.com>
2017-06-14 11:15:10 +0200

    Log-In Window

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -3,10 +3,25 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.UIManager;
+import javax.swing.JButton;
+import javax.swing.JProgressBar;
 
 public class MainWindow {
 
-	private JFrame frame;
+	private JFrame frmQuestionmark;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -16,7 +31,7 @@ public class MainWindow {
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
-					window.frame.setVisible(true);
+					window.frmQuestionmark.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +50,115 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmQuestionmark = new JFrame();
+		frmQuestionmark.setTitle("QuestionMark");
+		frmQuestionmark.setBounds(100, 100, 500, 300);
+		frmQuestionmark.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{478, 0};
+		gridBagLayout.rowHeights = new int[]{37, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		frmQuestionmark.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		panel.setBackground(Color.WHITE);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.anchor = GridBagConstraints.NORTH;
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		frmQuestionmark.getContentPane().add(panel, gbc_panel);
+		
+		JLabel lblWillkommenBeiQuestionmark = new JLabel("Willkommen bei QuestionMark");
+		lblWillkommenBeiQuestionmark.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel.add(lblWillkommenBeiQuestionmark);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(UIManager.getColor("CheckBox.background"));
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		frmQuestionmark.getContentPane().add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{87, 373, 0};
+		gbl_panel_1.rowHeights = new int[]{26, 26, 0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JLabel lblUserid = new JLabel("UserID:");
+		lblUserid.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_lblUserid = new GridBagConstraints();
+		gbc_lblUserid.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblUserid.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUserid.gridx = 0;
+		gbc_lblUserid.gridy = 0;
+		panel_1.add(lblUserid, gbc_lblUserid);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		panel_1.add(textField, gbc_textField);
+		textField.setColumns(15);
+		
+		JLabel lblPasswort = new JLabel("Passwort:");
+		lblPasswort.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_lblPasswort = new GridBagConstraints();
+		gbc_lblPasswort.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblPasswort.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPasswort.gridx = 0;
+		gbc_lblPasswort.gridy = 1;
+		panel_1.add(lblPasswort, gbc_lblPasswort);
+		
+		passwordField = new JPasswordField();
+		passwordField.setColumns(15);
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordField.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 1;
+		panel_1.add(passwordField, gbc_passwordField);
+		
+		JButton btnHelp = new JButton("Help");
+		btnHelp.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_btnHelp = new GridBagConstraints();
+		gbc_btnHelp.insets = new Insets(0, 0, 5, 5);
+		gbc_btnHelp.gridx = 0;
+		gbc_btnHelp.gridy = 2;
+		panel_1.add(btnHelp, gbc_btnHelp);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 2;
+		panel_1.add(btnNewButton, gbc_btnNewButton);
+		
+		JLabel lblFortschirtt = new JLabel("Fortschirtt:");
+		GridBagConstraints gbc_lblFortschirtt = new GridBagConstraints();
+		gbc_lblFortschirtt.insets = new Insets(0, 0, 0, 5);
+		gbc_lblFortschirtt.gridx = 0;
+		gbc_lblFortschirtt.gridy = 3;
+		panel_1.add(lblFortschirtt, gbc_lblFortschirtt);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(50);
+		GridBagConstraints gbc_progressBar = new GridBagConstraints();
+		gbc_progressBar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_progressBar.gridx = 1;
+		gbc_progressBar.gridy = 3;
+		panel_1.add(progressBar, gbc_progressBar);
 	}
 
 }
