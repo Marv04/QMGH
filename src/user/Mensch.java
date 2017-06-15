066552a86b77597f0257ad874c93fb303bb53921
manuel <manuel@10.20.241.9>
2017-06-15 14:28:22 +0200

    Das hier ist eine Änderung!

---
+++ src/user/Mensch.java
@@ -0,0 +1,25 @@
+package user;
+/**
+ * @author manuel Datum: 15.06.2017 Version 1.0
+ * 
+ */
+public class Mensch {
+	// Private Attribute für Objekten Menschen und für die veerbten Objekte
+	// Creator und Solver
+	private int humanID;
+	private String vorname;
+	private String nachname;
+	private String passwort;
+
+	// Methoden
+
+	public boolean isProperPasswort(String passwort) {
+		// Rückgabewert
+		boolean answer = false;
+
+		// Rückgabe der Methode
+		return answer;
+
+	}
+
+}
