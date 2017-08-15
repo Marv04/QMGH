dbaba5c5939ce681b6486c11d5527016eab4091b
Frequent Levi <Frequent Levi@Levi-HP.Speedport_W_724V_09011603_05_010>
2017-08-15 11:10:13 +0200

    Methode hat nun Rumpf mit Inhalt.

--- src/guiModules/FBCreationModul.java
+++ src/guiModules/FBCreationModul.java
@@ -1,6 +1,7 @@
 package guiModules;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import umfrage.Frage;
 import umfrage.Fragebogen;
@@ -10,6 +11,15 @@ public class FBCreationModul {
 	public static boolean createFB(Creator fbCreator, String titel, String exposee, String[][] frageDaten){
 		try{
 			ArrayList<Frage> frageList = new ArrayList<Frage>();
+			
+			ArrayList<String> antwortmoeglichkeitenShell;
+			for(int i = 0; i < frageDaten.length; i++){
+				antwortmoeglichkeitenShell = new ArrayList<String>();
+				for(int j = 0; j < frageDaten[2].length; j++){
+					antwortmoeglichkeitenShell.add(frageDaten[i][j]);
+				}
+				frageList.add(FCreationModul.createFrage(Integer.parseInt(frageDaten[i][0]), frageDaten[i][1], antwortmoeglichkeitenShell));
+			}
 			
 			fbCreator.addOwnedFragebogen(new Fragebogen(titel, exposee, frageList));
 		}catch(Exception e){
