9b59fdc2f6a73f177b2792be8dc9c058302233d5
Frequent Levi <Frequent Levi@Levi-HP.Speedport_W_724V_09011603_05_010>
2017-08-15 11:25:20 +0200

    Schnittstelle musste angepasst werden; Details auf WA

--- src/guiModules/FBCreationModul.java
+++ src/guiModules/FBCreationModul.java
@@ -8,17 +8,17 @@ import umfrage.Fragebogen;
 import user.Creator;
 
 public class FBCreationModul {
-	public static boolean createFB(Creator fbCreator, String titel, String exposee, String[][] frageDaten){
+	public static boolean createFB(Creator fbCreator, String titel, String exposee, String[][][] frageDaten){
 		try{
 			ArrayList<Frage> frageList = new ArrayList<Frage>();
 			
 			ArrayList<String> antwortmoeglichkeitenShell;
 			for(int i = 0; i < frageDaten.length; i++){
 				antwortmoeglichkeitenShell = new ArrayList<String>();
-				for(int j = 0; j < frageDaten[2].length; j++){
-					antwortmoeglichkeitenShell.add(frageDaten[i][j]);
+				for(int j = 0; j < frageDaten[i][2].length; j++){
+					antwortmoeglichkeitenShell.add(frageDaten[i][1][j]);
 				}
-				frageList.add(FCreationModul.createFrage(Integer.parseInt(frageDaten[i][0]), frageDaten[i][1], antwortmoeglichkeitenShell));
+				frageList.add(FCreationModul.createFrage(Integer.parseInt(frageDaten[i][0][0]), frageDaten[i][0][1], antwortmoeglichkeitenShell));
 			}
 			
 			fbCreator.addOwnedFragebogen(new Fragebogen(titel, exposee, frageList));
