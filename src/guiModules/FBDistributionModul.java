42d73015ee1366c16aa8a5a767eff3bed81494cc
Frequent Levi <Frequent Levi@Levi-HP.Speedport_W_724V_09011603_05_010>
2017-08-17 11:30:01 +0200

    Redundanzbeseitigung - GUI-Input-Anpassung nötig.

--- src/guiModules/FBDistributionModul.java
+++ src/guiModules/FBDistributionModul.java
@@ -1,18 +1,14 @@
 package guiModules;
 
 import umfrage.Fragebogen;
-import upper.containertier.Gesamtsystem;
 import user.Creator;
+import verwaltung.Kurs;
 
 public class FBDistributionModul {
 	//Hier müssen 2 Kurse (Indize 0, 1) vorhanden sein...
-	public static boolean distributeFB(Gesamtsystem targetSystem, boolean kursA, Fragebogen fbToBeDistributed, Creator requestingUser){
+	public static boolean distributeFB(Kurs targetKurs, Fragebogen fbToBeDistributed, Creator requestingUser){
 		try{
-			if(kursA){
-				requestingUser.distributeFragebogenTo(targetSystem.getAllKurse().get(0), fbToBeDistributed);
-			}else{
-				requestingUser.distributeFragebogenTo(targetSystem.getAllKurse().get(1), fbToBeDistributed);
-			}
+			requestingUser.distributeFragebogenTo(targetKurs, fbToBeDistributed);
 		}catch(Exception e){
 			return false;
 		}
